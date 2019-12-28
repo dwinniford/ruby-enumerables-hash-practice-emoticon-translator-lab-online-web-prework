b432d21b9afb29acc136a5cad905e17ce2fe7c18
darling-flag-8189 <dwinniford@gmail.com>
2019-12-28 21:21:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ require "yaml"
 def load_library(file)
   emoticons = YAML.load_file(file)
   new_emoticons = {
-    :get_meaning = {},
-    :get_emoticon = {}
+    :get_meaning => {},
+    :get_emoticon => {}
   }
   emoticons.each_with_object(new_emoticons) do |(meaning, emoticon_array), new_emoticons|
   new_key = meaning[1]
