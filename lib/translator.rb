ba9fc0f07a401faf125f8a7794ffb04115c3a414
darling-flag-8189 <dwinniford@gmail.com>
2019-12-28 21:28:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,9 @@ def load_library(file)
   }
   emoticons.each_with_object(new_emoticons) do |(meaning, emoticon_array), new_emoticons|
   japanese_emoticon = emoticon_array[1]
+  english_emoticon = emoticon_array[0]
   new_emoticons[:get_meaning][japanese_emoticon] = meaning
+  new_emoticons[:get_emoticon][english_emoticon] = japanese_emoticon
   end 
   new_emoticons
 end
